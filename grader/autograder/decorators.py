9307da6220637d4e0e9665d079b8afab63d33351
wongband <bmwong@ucdavis.edu>
2020-02-22 22:22:12 -0800

    Added more decorators and made it easier to retrieve progress objects

--- grader/autograder/decorators.py
+++ grader/autograder/decorators.py
@@ -1,9 +1,10 @@
 from flask import request
 from flask_jwt_extended import get_jwt_identity
 from functools import wraps
-from grader import guard
+from grader import bcrypt, guard
 from grader.autograder.schemas import user_login_schema
-from grader.models import Activity, ActivityProgress, CheckpointProgress, Student
+from grader.autograder.utils import get_activity_prog, get_checkpoint_prog
+from grader.models import Activity, Checkpoint, User
 
 
 # Decorator to check if a activity exists
@@ -22,12 +23,31 @@ def activity_exists(f):
     return wrap
 
 
+# Decorator to check if a checkpoint_prog exists
+def activity_prog_exists(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        data = request.form
+        username = data["username"]
+
+        # 0 index is checkpoint progress 1 index is activity progress
+        activity_prog = get_activity_prog(data["activity_id"], username)
+
+        if activity_prog:
+            return f(*args, **kwargs)
+        else:
+            return {
+                       "message": "ActivityProgress does not exist"
+                   }, 404
+
+    return wrap
+
+
 # Decorator to check if a checkpoint exists
 def checkpoint_exists(f):
     @wraps(f)
     def wrap(*args, **kwargs):
-        checkpoint = Activity.query.get(request.form["checkpoint_id"])
-
+        checkpoint = Checkpoint.query.get(request.form["checkpoint_id"])
         if checkpoint:
             return f(*args, **kwargs)
         else:
@@ -43,16 +63,53 @@ def checkpoint_prog_exists(f):
     @wraps(f)
     def wrap(*args, **kwargs):
         username = get_jwt_identity()
-        student = Student.query.filter_by(username=username).first()
-        activity_prog = ActivityProgress.query.filter_by(activity_id=request.form["activity_id"],
-                                                         student_id=student.id).first()
-        checkpoint_prog = CheckpointProgress.query.filter_by(activity_progress_id=activity_prog.id,
-                                                             checkpoint_id=request.form["checkpoint_id"]).first()
+
+        if not username:
+            data = request.form
+            username = data["username"]
+
+        checkpoint_prog = get_checkpoint_prog(request.form["activity_id"], request.form["checkpoint_id"], username)
+
         if checkpoint_prog:
             return f(*args, **kwargs)
         else:
             return {
                        "message": "CheckpointProgress does not exist"
+                   }, 404
+
+    return wrap
+
+
+# Decorator to check if a cli user exists
+def cli_user_exists(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        form_data = request.form
+        username = form_data["username"]
+        token = form_data["token"]
+        user = User.query.filter_by(username=username).first()
+        is_user = bcrypt.check_password_hash(user.token, token)
+
+        if is_user:
+            return f(*args, **kwargs)
+        else:
+            return {
+                       "message": "CLI User does not exist"
+                   }, 404
+
+    return wrap
+
+
+# Decorator to check if a checkpoint is an autograder checkpoint
+def is_autograder(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        checkpoint = Checkpoint.query.get(request.form["checkpoint_id"])
+        if checkpoint.checkpoint_type == "Autograder":
+            return f(*args, **kwargs)
+        else:
+            return {
+                       "message": "Checkpoint is not an Autograder checkpoint"
                    }, 404
 
     return wrap
@@ -80,4 +137,5 @@ def user_exists(f):
                 return {
                            "message": "User does not exist"
                        }, 404
+
     return wrap
